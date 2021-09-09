d14325f5bbd6ad029ff4a41ab3f535f9fcd2070d
hisashi <sanlike0911@gmail.com>
2021-09-10 08:30:22 +0900

    Fix: Missing files

--- build.bat
+++ build.bat
@@ -1,16 +1,14 @@
 @echo off
-cd /d %~dp0
+setlocal
+set CurrentDirectory="%~dp0"
 
-rem node-RED container name
-set nodeRedContainerName=nodeRED
-
-rem build and install
+rem build and install and npm run start
 npm run build & ^
-copy /Y src\*.html nodes & ^
-xcopy /Y /S /E dist\* nodes\* & ^
-xcopy /Y /S /E src\locales\ nodes\locales\* & ^
-xcopy /Y /S /E nodes\* test\data\your-node-red\nodes\* & ^
-copy /Y package.json test\data\your-node-red\ & ^
-docker exec -it %nodeRedContainerName% bash -c "cd /data/ && npm install ./your-node-red/ && exit" & ^
-docker restart %nodeRedContainerName%
-pause
+xcopy /Y /S /E /I /EXCLUDE:build-exclude-filelist src\nodes\* nodes\* & ^
+xcopy /Y /S /E /I dist\* nodes\* & ^
+xcopy /Y /S /E /I nodes\* data\your-node\nodes\* & ^
+xcopy /Y /I package.json data\your-node\* & ^
+cd data/ & ^
+npm install .\your-node & ^
+cd .. & ^
+if "%1" EQU "-r" ( npm run start )
